3156148ba559ef043812c2b62a83c7446ea3dcf1
ianzuinelli <ignacioanz2007@gmail.com>
2025-06-29 21:48:15 -0300

    Add files via upload

---
+++ Portfolio/botones_sonidos.js
@@ -0,0 +1,27 @@
+const soundButtons = document.querySelectorAll('.sound-button');
+const audioPlayer = document.getElementById('audioPlayer');
+
+ function reproducirSonido() {
+      const audio = document.getElementById('miAudio');
+      audio.currentTime = 0; // Reinicia el audio si ya se estaba reproduciendo
+      audio.play();}
+  
+  function reproducirSonido2() {
+      const audio = document.getElementById('miAudio2');
+      audio.currentTime = 0; // Reinicia el audio si ya se estaba reproduciendo
+      audio.play();}
+  
+  function reproducirSonido3() {
+      const audio = document.getElementById('miAudio3');
+      audio.currentTime = 0; // Reinicia el audio si ya se estaba reproduciendo
+      audio.play();}
+  
+  function reproducirSonido4() {
+      const audio = document.getElementById('miAudio4');
+      audio.currentTime = 0; // Reinicia el audio si ya se estaba reproduciendo
+      audio.play();}
+    
+  function reproducirSonido5() {
+      const audio = document.getElementById('miAudio5');
+      audio.currentTime = 0; // Reinicia el audio si ya se estaba reproduciendo
+      audio.play();}
